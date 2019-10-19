c3a6671e946c497541c9e0480ec10b906eb699ba
Matthew Schlegel <mkschleg@cedar5.cedar.computecanada.ca>
2019-10-18 19:55:01 -0700

    Merging with cedar

--- parallel/four_rooms_cont_study.jl
+++ parallel/four_rooms_cont_study.jl
@@ -1,10 +1,12 @@
-#!/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx512/Compiler/gcc7.3/julia/1.1.0/bin/julia
-#SBATCH -o four_rooms_norm_is.out # Standard output
-#SBATCH -e four_rooms_norm_is.err # Standard error
+#!/cvmfs/soft.computecanada.ca/easybuild/software/2017/avx2/Compiler/gcc7.3/julia/1.1.0/bin/julia
+#SBATCH --mail-user=mkschleg@ualberta.ca
+#SBATCH --mail-type=ALL
+#SBATCH -o four_rooms_cont_exp.out # Standard output
+#SBATCH -e four_rooms_cont_exp.err # Standard error
 #SBATCH --mem-per-cpu=2000M # Memory request of 2 GB
-#SBATCH --time=4:00:00 # Running time of 6 hours
-#SBATCH --ntasks=32
-#SBATCH --account=def-whitem
+#SBATCH --time=24:00:00 # Running time of 12 hours
+#SBATCH --ntasks=128
+#SBATCH --account=rrg-whitem
 
 using Pkg
 Pkg.activate(".")
@@ -12,7 +14,7 @@ Pkg.activate(".")
 using Reproduce
 using Logging
 
-const save_loc = "four_rooms_cont_exp_var_red_study"
+const save_loc = "/home/mkschleg/scratch/four_rooms_cont_exp"
 const exp_file = "experiment/four_rooms_cont.jl"
 const exp_module_name = :FourRoomsContExperiment
 const exp_func_name = :main_experiment
@@ -20,12 +22,12 @@ const exp_func_name = :main_experiment
 const alphas = [collect(0.0:0.01:0.1)..., 0.2, 0.3, 0.4, 0.5, 0.75, 1.0]
 const policies = ["random_state_variant", "random_state_weight_variant", "uniform"]
 const gvfs = ["collide_down", "favored_down"]
-const batchsizes = [1, 8, 16]
+const batchsizes = [8, 16]
 const train_gaps = [1, 2, 3, 4, 5, 6, 7, 8, 16, 24, 32, 48, 64, 80, 96, 114, 128, 160, 192, 224, 256]
 # const train_gaps = [80, 96, 114, 128, 160, 192, 224, 256]
 const warm_up = 1000
 const buffersize = 15000
-const numsteps = 1500
+const numsteps = 250000
 
 function make_arguments(args::Dict)
     new_args=["--policy", args["policy"],
@@ -33,7 +35,7 @@ function make_arguments(args::Dict)
               "--train_gap", args["train_gap"],
               "--batchsize", args["batchsize"],
               "--run", args["run"],
-              "--alphas", string.(alphas./parse(Int64, args["batchsize"]))...]
+              "--alphas", string.(alphas)...]
     return new_args
 end
 
@@ -46,7 +48,7 @@ function main()
         default=1
         "--numruns"
         arg_type=Int64
-        default=10
+        default=25
         "--saveloc"
         arg_type=String
         default=save_loc
@@ -68,7 +70,7 @@ function main()
     ])
     arg_list = ["policy", "gvf", "train_gap", "batchsize", "run"]
 
-    alg_list = ["--normis", "--ir", "--bcir", "--wisbatch",
+    alg_list = ["--normis", "--is", "--ir", "--bcir", "--wisbatch",
                 "--vtrace", "--clip_value_perc", "0.5", "0.9", "1.0", "--clip_value", "1.0"]
 
     static_args = [alg_list;
